ae7e315b2cae031c5019d4cf5a917464dad9de61
Jorge Samuel Teixeira Jordão <116199476+JorgeSTJordao@users.noreply.github.com>
2024-09-21 16:27:40 -0300

    :new: Added rolagem task

--- example.c
+++ example.c
@@ -1,120 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "FreeRTOS.h"
 #include "task.h"
 #include "basic_io.h"
 
+// Definindo o tamanho padrao da pilha para facilitar manutencao
+#define STACK_SIZE 1000
+
+// Variaveis que simulam a velocidade dos motores
+int motor_0 = 0;
+int motor_1 = 0;
+int motor_2 = 0;
+int motor_3 = 0;
+
 //Protótipos das tarefas
-void temperature(void* pvParameters);
-void saturation(void* pvParameters);
-void heartbeat(void* pvParameters);
 
-//Tarefa para medir a temperatura
-void temperature(void* pvParameters)
-{
-	char* dados = (char*)pvParameters;
-	float pTemperature;
-	
-	//Início do loop
-	for (;;)
+//Função de arfagem
+void taskArfagem(void* pvParameters);
+//Função de rolagem
+void taskRolagem(void* pvParameters);
 
-	{
-		//Variável que recebe os valores entre 34 até 41 de forma aleatória
-		pTemperature = 34.0f + ((float)rand()) / ((float)RAND_MAX) * 6.0f;
-
-		printf("%s %.2f", dados, pTemperature);
-
-		//Hipotermia com temperatura menor que 35
-		if (pTemperature < 35.0f) {
-			printf("HIPOTERMIA");
-		}
-		//Febre com temperatura maior que 37.5
-		else if (pTemperature > 37.5f) {
-			printf("FEBRE");
-		}
-
-		//Tarefa bloqueada por 1s
-		printf("\n");
-		vTaskDelay(1000);
-	}
-
-	//Tarefa não mais disponível na memória RAM
-	vTaskDelete(NULL);
-}
-
-//Tarefa para medir a saturação
-void saturation(void* pvParameters)
-{
-	char* dados = (char*)pvParameters;
-	float pSaturation;
-	
-
-	//Início do loop
-	for (;;)
-	{
-		pSaturation = 80.0f + ((float)rand()) / (float)(RAND_MAX) * 20.0f;
-
-		vPrintStringAndNumber(dados, pSaturation);
-
-		if (pSaturation < 90.0f) {
-			printf("SATURAÇÃO BAIXA");
-		}
-
-		//Tarefa bloequada por 1s
-		printf("\n");
-		vTaskDelay(1000);
-	}
-
-	//Tarefa não mais disponível na memória RAM
-	vTaskDelete(NULL);
-}
-
-//Tarefa para medir os batimentos
-void heartbeat(void* pvParameters)
-{
-	char* dados = (char*)pvParameters;
-	float pHeartBeat;
-
-	//Início do loop
-	for (;;)
-
-	{
-		pHeartBeat = 20.0f + ((float)rand()) / (float)(RAND_MAX) * 120.0f;
-
-		vPrintStringAndNumber(dados, pHeartBeat);
-
-		if (pHeartBeat < 50.0f) {
-			printf("BATIMENTO CARDÍACO BAIXO");
-		}
-		else if (pHeartBeat > 90.0f) {
-			printf("BATIMENTO CARDÍACO ALTO");
-		}
-
-		//Tarefa bloequada por 1s
-		printf("\n");
-		vTaskDelay(1000);
-	}
-
-	//Tarefa não mais disponível na memória RAM
-	vTaskDelete(NULL);
-}
 
 int main_(void)
 {
 
-	//Na respectiva ordem
-	//pvTaskCode: pointeiro da tarefa que implementa a função
-	//pcName: nome descritivo para a tarefa
-	//usStackDepth: espaço alocado na memória RAM em bytes (bloco da tarefa)
-	//pvParameters: parâmetro do tipo 'pointer do void (void*)'
-	//uxPriority: prioridade da tarefa
-	//pxCreatedTask: usado para passar um handle para a tarefa criada
-	xTaskCreate(temperature, "Temperature", 1000, (void*)"- Temperatura ", 1, NULL);
-	xTaskCreate(saturation, "Saturation", 1000, (void*)"- Saturação ", 1, NULL);
-	xTaskCreate(heartbeat, "HeartBeat", 1000, (void*)"- Batimentos ", 1, NULL);
+	xTaskCreate(taskArfagem, "taskArfagem", STACK_SIZE, (void*)"frente", 1, NULL);
+	xTaskCreate(taskRolagem, "taskRolagem", STACK_SIZE, (void*)"direita", 1, NULL);
 
-	//Início do escalonamento RTOS
+
 	vTaskStartScheduler();
 
 	for (;; );
 	return 0;
+}
+
+// Funcao que define a funcionalidade de arfagem do quadricoptero
+void taskArfagem(void* pvParameters)
+{
+
+	char* direction = (char*)pvParameters;
+
+	//Início do loop
+	for (;;)
+
+	{
+		//Direção vertical
+		//Sentido: frente
+		if (strcmp(direction, "frente") == 0) {
+			motor_2 += 25;
+			motor_3 += 25;
+			motor_0 -= 25;
+			motor_1 -= 25;
+
+		}
+		//Sentido: trás
+		else if (strcmp(direction, "trás") == 0) {
+			motor_2 -= 25;
+			motor_3 -= 25;
+			motor_0 += 25;
+			motor_1 += 25;
+		}
+
+		vPrintString(direction);
+		printf("ARFAGEM | motor 0: %d |  motor 1: %d | motor 2: %d |  motor 3: %d", motor_0, motor_1, motor_2, motor_3);
+
+		// Simula o delay de 40ms para a rolagem
+		vTaskDelay(pdMS_TO_TICKS(40));
+	}
+
+	//Tarefa de arfagem deletada
+	vTaskDelete(NULL);
+}
+
+// Funcao que define a funcionalidade de rolagem do quadricoptero
+void taskRolagem(void* pvParameters) {
+	char* direction = (char*)pvParameters;
+
+	for (;;) {
+		if (strcmp(direction, "direita") == 0) {
+			// Alteracao para rolagem para a direita
+			motor_0 += 50;
+			motor_3 += 50;
+			motor_1 -= 50;
+			motor_2 -= 50;
+		}
+		else if (strcmp(direction, "esquerda") == 0) {
+			// Alteracao para rolagem para a esquerda
+			motor_0 -= 50;
+			motor_3 -= 50;
+			motor_1 += 50;
+			motor_2 += 50;
+		}
+
+		vPrintString(direction);
+		printf("ROLAGEM | motor 0: %d |  motor 1: %d | motor 2: %d |  motor 3: %d", motor_0, motor_1, motor_2, motor_3);
+
+		// Simula o delay de 20ms para a rolagem
+		vTaskDelay(pdMS_TO_TICKS(20));
+	}
+
+	// Exclui a tarefa
+	vTaskDelete(NULL);
 }
